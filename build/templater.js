944729a1b648fa8c878707b3d28ffae6f63370ca
Domenic Denicola <d@domenic.me>
2015-01-25 01:03:10 -0500

    Template-ify lectures!
    Also fixed an issue where edits to template files (in particular partials) would not trigger a rebuild during watch.
    
    Closes #5!

--- build/templater.js
+++ build/templater.js
@@ -1,14 +1,20 @@
 const path = require('path');
 const yaml = require('js-yaml');
 const fs = require('fs');
-const handlebars = require('handlebars');
+const handlebarsFactory = require('handlebars');
 const moment = require('moment');
+const assert = require('assert');
 
-handlebars.registerHelper('date', dateHandlebarsHelper);
-registerPartials();
+const SLIDES_AND_NOTES = 'Slides and Notes';
+const READING_PRE = 'Reading (Pre)';
+const READING_SUPPLEMENTAL = 'Reading (Supplemental)';
 
 export default function (input, output) {
-    const template = compileTemplate(input);
+    const handlebars = handlebarsFactory.create();
+    registerHelpers(handlebars);
+    registerPartials(handlebars);
+
+    const template = compileTemplate(handlebars, input);
     const documents = parseDocuments();
     fs.writeFileSync(output, template(documents));
 }
@@ -16,17 +22,29 @@ export default function (input, output) {
 // Uncomment to see documents; useful while tweaking the templates
 // console.log(require("util").inspect(documents, { depth: Infinity }));
 
-function compileTemplate(input) {
+function compileTemplate(handlebars, input) {
     return handlebars.compile(fs.readFileSync(input, { encoding: 'utf-8' }));
 }
 
-function registerPartials() {
-    const assignmentsFilename = path.resolve(__dirname, '../templates/assignments.hbs');
-    handlebars.registerPartial('assignments', fs.readFileSync(assignmentsFilename, { encoding: 'utf-8' }));
-}
-
 function parseDocuments() {
-    const topics = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../data/topics.yml')));
+    const lectures = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../data/lectures.yml')));
+
+    // Pull Slides and Notes, Reading (Pre), and Reading (Supplemental) from the topics up to the top level:
+    for (const lecture of lectures) {
+        Object.keys(lecture.Events).forEach(eventDate => {
+            const event = lecture.Events[eventDate];
+
+            ensureArrayExists(event, SLIDES_AND_NOTES);
+            ensureArrayExists(event, READING_PRE);
+            ensureArrayExists(event, READING_SUPPLEMENTAL);
+
+            Object.keys(event.Topics).forEach(topic => {
+                copyArrayInto(event.Topics[topic], event, SLIDES_AND_NOTES);
+                copyArrayInto(event.Topics[topic], event, READING_PRE);
+                copyArrayInto(event.Topics[topic], event, READING_SUPPLEMENTAL);
+            });
+        });
+    }
 
     let assignmentsFrontmatter, assignments;
     let i = 0;
@@ -44,9 +62,43 @@ function parseDocuments() {
         ++i;
     });
 
-    return { topics, assignmentsFrontmatter, assignments };
+    return { lectures, assignmentsFrontmatter, assignments };
 }
 
-function dateHandlebarsHelper(date) {
-    return moment.utc(date).format('MMMM Do');
+function registerPartials(handlebars) {
+    const assignmentsFilename = path.resolve(__dirname, '../templates/assignments.hbs');
+    handlebars.registerPartial('assignments', fs.readFileSync(assignmentsFilename, { encoding: 'utf-8' }));
+
+    const lecturesFilename = path.resolve(__dirname, '../templates/lectures.hbs');
+    handlebars.registerPartial('lectures', fs.readFileSync(lecturesFilename, { encoding: 'utf-8' }));
+}
+
+function registerHelpers(handlebars) {
+    handlebars.registerHelper('date', d => moment.utc(d).format('MMMM Do'));
+    handlebars.registerHelper('shortDate', d => moment.utc(d).format('MMM D'));
+    handlebars.registerHelper('maybeLink', v => {
+        if (typeof v === 'string') {
+            return v;
+        }
+
+        assert (typeof v === 'object' && v !== null, 'Links must be either strings or objects');
+
+        const keys = Object.keys(v);
+        assert(keys.length === 1, 'Link objects must have a single key');
+        const key = keys[0];
+
+        return new handlebars.SafeString('<a href="' + v[key] + '">' + key + '</a>');
+    });
+}
+
+function ensureArrayExists(obj, prop) {
+    if (!(prop in obj)) {
+        obj[prop] = [];
+    }
+}
+
+function copyArrayInto(source, dest, keyName) {
+    if (source && source[keyName]) {
+        dest[keyName].push(...source[keyName]);
+    }
 }
