d4e649aa01a1d1a8c5e82d163d0d56b5e0a6ec4c
Domenic Denicola <d@domenic.me>
2015-02-14 17:32:27 -0500

    If templating fails, delete index.html
    This should allow anyone using the watcher to notice more quickly that the templating failed.
    
    Hopefully this is enough to close #36 and prevent it in the future.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 import templater from './build/templater.js';
+const fs = require('fs');
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const gulpStylus = require('gulp-stylus');
 
 const STYLUS = 'styles/*.styl';
@@ -10,9 +12,17 @@ const DATA = 'data/*.yml';
 const OUTPUT_HTML = 'index.html';
 const OUTPUT_STYLES = 'styles/';
 
-gulp.task('template', () =>
-    templater(MAIN_TEMPLATE, OUTPUT_HTML)
-);
+gulp.task('template', () => {
+    try {
+        templater(MAIN_TEMPLATE, OUTPUT_HTML)
+    } catch (e) {
+        // Delete the index.html file to make it clear that the templating failed
+        deleteIfExists(OUTPUT_HTML);
+
+        gutil.log(gutil.colors.red(`Deleting ${OUTPUT_HTML} since templating failed`));
+        throw e;
+    }
+});
 
 gulp.task('stylus', () =>
     gulp.src(STYLUS)
@@ -23,7 +33,16 @@ gulp.task('stylus', () =>
 gulp.task('build', ['template', 'stylus']);
 
 gulp.task('watch', ['template', 'stylus'], () => {
-    gulp.watch([TEMPLATES, DATA], ['template'])
+    gulp.watch([TEMPLATES, DATA], ['template']);
     gulp.watch(STYLUS, ['stylus'])
 });
 
+function deleteIfExists(filename) {
+    try {
+        fs.unlinkSync(filename);
+    } catch (e) {
+        if (e.code !== 'ENOENT') {
+            throw e;
+        }
+    }
+}
